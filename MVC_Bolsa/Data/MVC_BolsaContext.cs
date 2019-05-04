91c725908a49bdb8fff348d2acbd25757bf893e0
Rafael Araujo Badaro <1071746@sga.pucminas.br>
2019-05-04 13:08:13 -0300

    Inicio

---
+++ MVC_Bolsa/Data/MVC_BolsaContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVC_Bolsa.Models
+{
+    public class MVC_BolsaContext : DbContext
+    {
+        public MVC_BolsaContext (DbContextOptions<MVC_BolsaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVC_Bolsa.Models.Acao> Acao { get; set; }
+    }
+}
---
+++ MVC_Bolsa/Models/Acao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Bolsa.Models
+{
+    public class Acao
+    {
+
+        public long ID { get; set; }
+
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [StringLength(100, ErrorMessage = "O nome não pode exceder 100 caracteres")]
+        public string Nome { get; set; }
+        [Display(Name = "Preço")]
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        public decimal Preco { get; set; }
+    }
+}
