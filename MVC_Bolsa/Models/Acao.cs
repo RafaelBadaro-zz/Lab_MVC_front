13d6e2e5fd8c8b75c262dc02c177012a720516be
Ez <ezequiel_carvalhos@outlook.com>
2019-05-17 22:13:08 -0300

    Traduções de Alguns trechos

--- MVC_Bolsa/Models/Acao.cs
+++ MVC_Bolsa/Models/Acao.cs
@@ -14,11 +14,11 @@ namespace MVC_Bolsa.Models
 
         [Display(Name = "Nome")]
         [Required(ErrorMessage = "Este campo é obrigatório")]
-        [StringLength(100, ErrorMessage = "O nome não pode exceder 100 caracteres")]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "O nome não deve ter mais de 8 caracteres")]
         public string Nome { get; set; }
         [Display(Name = "Preço")]
         [Required(ErrorMessage = "Este campo é obrigatório")]
-        [Column(TypeName = "decimal(18,2)")]
+        [Column(TypeName = "decimal(18.2)")]
         public decimal Preco { get; set; }
     }
 }
